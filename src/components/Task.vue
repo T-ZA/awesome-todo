913ee1a1958b7c69c2e6c5f904e9cbcf9ce2de58
T-ZA <ricky.sanders62@gmail.com>
2019-07-28 16:35:27 -0500

    Section 4: Vue Lists and Child Components

---
+++ src/components/Task.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <div>
+      <slot></slot> <!-- slot to be received from parent component -->
+      {{index}}
+    </div>
+    <small>{{task.dueDate}} @ {{task.dueTime}}</small>
+
+    <!-- Events example (tell parent component to delete task in the task list -->)
+    <button @click="$emit('delete-task', index)">X</button>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: ['task', 'index']
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
