124329bc0e2dbe0c57866b50cf4598cff4cd5d81
T-ZA <ricky.sanders62@gmail.com>
2019-08-09 04:34:37 -0500

    Section 7: Vuex - State Management - Setting Up

---
+++ src/store/store-tasks.js
@@ -0,0 +1,72 @@
+// Data
+const state = {
+  tasks: {
+    'ID1' : {
+      name: 'Task 1',
+      completed: false,
+      dueDate: '08/15/2019',
+      dueTime: '00:00'
+    },
+    'ID2' : {
+      name: 'Task 2',
+      completed: false,
+      dueDate: '08/15/2019',
+      dueTime: '00:00'
+    },
+    'ID3' : {
+      name: 'Task 3',
+      completed: false,
+      dueDate: '08/15/2019',
+      dueTime: '00:00'
+    }
+  }
+  // tasks: [
+  //   {
+  //     id: 1,
+  //     name: 'Task 1',
+  //     completed: false,
+  //     dueDate: '08/15/2019',
+  //     dueTime: '00:00'
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Task 2',
+  //     completed: false,
+  //     dueDate: '08/15/2019',
+  //     dueTime: '00:00'
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Task 3',
+  //     completed: false,
+  //     dueDate: '08/15/2019',
+  //     dueTime: '00:00'
+  //   }
+  // ]
+}
+
+// Synchronous fuinctions to modify state
+const mutations = {
+
+}
+
+// Can be asynchronous
+// Called by components to modify state
+const actions = {
+
+}
+
+// Retrieve data from state
+const getters = {
+  tasks: (state) => {
+    return state.tasks
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+}
