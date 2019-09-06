41d509f311ce5f0051f1c9e036611730e122f8c9
T-ZA <ricky.sanders62@gmail.com>
2019-09-05 20:02:26 -0500

    Section 12 - Add a Search Bar

---
+++ src/components/Tasks/Tools/Search.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-input
+    v-model="searchField"
+    outlined
+    label="Search Tasks"
+    class="col"
+    type="search"
+  >
+    <template v-slot:append>
+      <!-- Clear Input -->
+      <q-icon
+        v-if="searchField !== ''"
+        name="close"
+        class="cursor-pointer"
+        @click="searchField = ''"
+      />
+
+      <!-- Search Icon -->
+      <q-icon name="search" />
+    </template>
+  </q-input>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+  export default {
+    computed: {
+      ...mapState('tasks', ['search']),
+      searchField: {
+        get () {
+          return this.search
+        },
+        set (value) {
+          this.setSearch(value)
+        }
+      }
+    },
+    methods: {
+      ...mapActions('tasks', ['setSearch'])
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
